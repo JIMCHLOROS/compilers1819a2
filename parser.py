332e1c967a058a76b2f1f3cd6b4f4574e69f79f1
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2019-05-24 21:49:01 +0300

    update

--- parser.py
+++ parser.py
@@ -1,6 +1,147 @@
-
 import plex
 
 
-# ... συμπληρώστε τον κώδικά σας για τον συντακτικό αναλυτή - αναγνωριστή της γλώσσας ...
+class ParseError(Exception):
+    pass
 
+
+class myParser:
+    def __init__(self):
+        DIGIT = plex.Range("09")
+        BINARY_DIGIT = plex.Range("01")
+        LETTER = plex.Range('azAZ')
+        BINARY_TOKEN = plex.Rep1(BINARY_DIGIT)
+        IDENTIFIER_TOKEN_OPERATOR = LETTER + plex.Rep(LETTER | DIGIT)
+        AND_TOKEN = plex.Str("and")
+        OR_TOKEN = plex.Str("or")
+        XOR_TOKEN = plex.Str("xor")
+        EQUALITY_OPERATOR = plex.Str("=")
+        OPEN_PARENTHESES = plex.Str("(")
+        CLOSE_PARENTHESES = plex.Str(")")
+        PRINT_TOKEN = plex.Str("print")
+        SPACE = plex.Any(" \n\t")
+
+        self.LEXICON = plex.Lexicon([(SPACE, plex.IGNORE),
+                                     (BINARY_TOKEN, "binary"),
+                                     (AND_TOKEN, "and"),
+                                     (OR_TOKEN, "or"),
+                                     (XOR_TOKEN, "xor"),
+                                     (EQUALITY_OPERATOR, "="),
+                                     (PRINT_TOKEN, "print"),
+                                     (OPEN_PARENTHESES, "("),
+                                     (CLOSE_PARENTHESES, ")"),
+                                     (IDENTIFIER_TOKEN_OPERATOR, "id")])
+
+    def create_scanner(self, fp):
+        self.SCANNER = plex.Scanner(self.LEXICON, fp)
+        self.LA, self.TEXT = self.next_token()
+
+    def next_token(self):
+        return self.SCANNER.read()
+
+    def match(self, token):
+        if self.LA == token:
+            self.LA, self.TEXT = self.next_token()
+        else:
+            raise ParseError("{} not the same as {}!".format(self.LA, token))
+
+    def parse(self, fp):
+        self.create_scanner(fp)
+        self.stmt_list()
+
+    def stmt_list(self):
+        if self.LA in ("id", "print"):
+            self.stmt()
+            self.stmt_list()
+        elif self.LA == None:
+            return
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def stmt(self):
+        if self.LA == "id":
+            self.match("id")
+            self.match("=")
+            self.expr()
+        elif self.LA == "print":
+            self.match("print")
+            self.expr()
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def expr(self):
+        if self.LA in ("(", "id", "binary"):
+            self.term()
+            self.term_tail()
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def term_tail(self):
+        if self.LA == "xor":
+            self.match("xor")
+            self.term()
+            self.term_tail()
+        elif self.LA in ("id", "print", ")", None):
+            return
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def term(self):
+        if self.LA in ("(", "id", "binary"):
+            self.atom()
+            self.atom_tail()
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def atom_tail(self):
+        if self.LA == "or":
+            self.match("or")
+            self.atom()
+            self.atom_tail()
+        elif self.LA in ("xor", "id", "print", ")", None):
+            return
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def atom(self):
+        if self.LA in ("(", "id", "binary"):
+            self.factor()
+            self.factor_tail()
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def factor_tail(self):
+        if self.LA == "and":
+            self.match("and")
+            self.factor()
+            self.factor_tail()
+        elif self.LA in ("xor", "or", "id", "print", ")", None):
+            return
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+    def factor(self):
+        if self.LA == "(":
+            self.match("(")
+            self.expr()
+            self.match(")")
+        elif self.LA == "id":
+            self.match("id")
+        elif self.LA == "binary":
+            self.match("binary")
+        else:
+            raise ParseError("{} was not what i was expecting!".format(self.LA))
+
+
+parser = myParser()
+with open("test.txt") as fp:
+    parser.parse(fp)
+
+"""
+A -> ....By
+    FOLLOW(B) <- FIRST(y)
+A -> ....B
+    FOLLOW(B) <- FOLLOW(A)
+A -> ....By (<- y -> ε)
+    FOLLOW(B) <- FOLLOW(A)
+"""
--- runner.py
+++ runner.py
@@ -1,6 +1,143 @@
-
 import plex
 
 
-# ... συμπληρώστε τον κώδικά σας για τον συντακτικό αναλυτή - διερμηνευτή της γλώσσας ...
+class ParseError(Exception):
+    pass
 
+
+class RunError(Exception):
+    pass
+
+
+class myParser:
+
+    def __init__(self):
+        DIGIT = plex.Range("09")
+        BINARY_DIGIT = plex.Range("01")
+        LETTER = plex.Range('azAZ')
+        BINARY_TOKEN = plex.Rep1(BINARY_DIGIT)
+        IDENTIFIER_TOKEN_OPERATOR = LETTER + plex.Rep(LETTER | DIGIT)
+        AND_TOKEN = plex.Str("and")
+        OR_TOKEN = plex.Str("or")
+        XOR_TOKEN = plex.Str("xor")
+        EQUALITY_OPERATOR = plex.Str("=")
+        OPEN_PARENTHESES = plex.Str("(")
+        CLOSE_PARENTHESES = plex.Str(")")
+        PRINT_TOKEN = plex.Str("print")
+        SPACE = plex.Any(" \n\t")
+
+        self.LEXICON = plex.Lexicon([(SPACE, plex.IGNORE),
+                                     (BINARY_TOKEN, "binary"),
+                                     (AND_TOKEN, "and"),
+                                     (OR_TOKEN, "or"),
+                                     (XOR_TOKEN, "xor"),
+                                     (EQUALITY_OPERATOR, "="),
+                                     (PRINT_TOKEN, "print"),
+                                     (OPEN_PARENTHESES, "("),
+                                     (CLOSE_PARENTHESES, ")"),
+                                     (IDENTIFIER_TOKEN_OPERATOR, "id")])
+
+        self.ST = {}
+
+    def create_scanner(self, fp):
+        self.SCANNER = plex.Scanner(self.LEXICON, fp)
+        self.LA, self.TEXT = self.next_token()
+
+    def next_token(self):
+        return self.SCANNER.read()
+
+    def match(self, TOKEN):
+        # print(self.LA, TOKEN)
+        if self.LA == TOKEN:
+            self.LA, self.TEXT = self.next_token()
+            # print(self.LA, self.TEXT)
+        else:
+            raise ParseError("self.LA not the same as token!")
+
+    def parse(self, fp):
+        self.create_scanner(fp)
+        self.stmt_list()
+
+    def stmt_list(self):
+        if self.LA in ("id", "print"):
+            self.stmt()
+            self.stmt_list()
+        elif self.LA == None:
+            return
+        else:
+            raise ParseError("Didnt get 'id' or 'print' token!")
+
+    def stmt(self):
+        if self.LA == "id":
+            varname = self.TEXT
+            self.match("id")
+            self.match("=")
+            self.ST[varname] = self.expr()
+        elif self.LA == "print":
+            self.match("print")
+            print(self.expr())
+        else:
+            raise ParseError("Didnt get what i was expecting!")
+
+    def expr(self):
+        if self.LA in ("(", "id", "binary"):
+            t = self.term()
+            while self.LA == "xor":
+                self.match("xor")
+                t2 = self.term()
+                t ^= t2
+            if self.LA in ("id", "print", ")", None):
+                return t
+            raise ParseError("Didn't get what i was expecting!")
+        else:
+            raise ParseError("Didnt get what i was expecting!")
+
+    def term(self):
+        if self.LA in ("(", "id", "binary"):
+            f = self.factor()
+            while self.LA == "or":
+                self.match("or")
+                f2 = self.factor()
+                f = f | f2
+            if self.LA in ("xor", "id", "print", ")", None):
+                return f
+            raise ParseError("Didnt get what i was expecting!")
+        else:
+            raise ParseError("Didnt get what i was expecting!")
+
+    def atom(self):
+        if self.LA in ("(", "id", "binary"):
+            f = self.factor()
+            while self.LA == "and":
+                self.match("and")
+                f2 = self.factor()
+                f += f2
+            if self.LA in ("xor", "or", "id", "print", ")", None):
+                return f
+            raise ParseError("Didnt get what i was expecting!")
+        else:
+            raise ParseError("Didnt get what i was expecting!")
+
+    def factor(self):
+        if self.LA == "(":
+            self.match("(")
+            e = self.expr()
+            self.match(")")
+            return e
+        elif self.LA == "id":
+            varname = self.TEXT
+            self.match("id")
+            if varname in self.ST:
+                return self.ST[varname]
+            raise RunError("Didn't find the value in the Dictionary.")
+        elif self.LA == "binary":
+            value = float(self.TEXT)
+            self.match("binary")
+            return value
+        else:
+            raise ParseError("Didnt get what i was expecting!")
+
+
+parser = myParser()
+with open("test.txt") as fp:
+    parser.parse(fp)
